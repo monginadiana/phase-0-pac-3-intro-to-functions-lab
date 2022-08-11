385ec3c7b62030f7c1a227f4dc45d6359257a8ce
monginadiana <dianahmongina2@gmail.com>
2022-08-11 12:21:15 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+    return string.toUpperCase()
+}
+console.log (shout('Diana'))
+
+function whisper(string){
+    return string.toLowerCase()
+}
+console.log (whisper('Diana'))
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase === string);{
+        return "I can\'t hear you!";
+    }
+    if (string.toUpperCase === string);{
+        return "YES INDEED";
+    }
+    if (string === "Let\'s have dinner together!");{
+        return  "I would love to!"
+    }
+   
+    }
+
+
+    
